02cc910d9c22fe7cf24aaf18c7889d9401515cff
Stu <stuart.feng@icloud.com>
2021-10-16 13:32:37 -0400

    Add the solution to 4sum

---
+++ Arrays/4sum.js
@@ -0,0 +1,45 @@
+// Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+// 0 <= a, b, c, d < n
+// a, b, c, and d are distinct.
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+
+// You may return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+ var fourSum = function(nums, target) {
+    nums.sort(function(a, b) {
+        return a - b;
+    });
+    const result = [];
+    
+    for(var i = 0; i < nums.length - 3; i++) {
+        for(var j = i + 1; j < nums.length - 2; j++) {
+            var low = j + 1;
+            var high = nums.length - 1;
+
+            while(low < high) {
+                const sum = nums[i] + nums[j] + nums[low] + nums[high];
+                if(sum === target) {
+                    result.push([nums[i], nums[j], nums[low], nums[high]])
+                    while(nums[low] === nums[low + 1]) low++;
+                    while(nums[high] === nums[high - 1]) high--;
+                    low++;
+                    high--;
+                } else if(sum < target) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }   
+            while(nums[j] === nums[j + 1]) j++;
+        }   
+        while(nums[i] === nums[i + 1]) i++;
+    }
+    
+    return result
+};
