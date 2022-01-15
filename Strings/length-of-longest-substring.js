445fdebfba349c820c19f66d76b6804caf144a6c
Stu <stuart.feng@icloud.com>
2022-01-14 20:17:08 -0500

    Update length-of-longest-substring.js

--- Strings/length-of-longest-substring.js
+++ Strings/length-of-longest-substring.js
@@ -6,28 +6,24 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-	let characters = {};
-	let start = 0;
-	let current_len = 0;
-	let i = 0;
+	var sLen = s.length,
+		maxLen = 0,
+		maxStr = "",
+		tmpStr,
+		posIndex,
+		i;
 
-	// check empty string
-	if (s.length === 0) {
-		return 0;
-	}
+	for (i = 0; i < sLen; i++) {
+		tmpStr = s[i];
+		posIndex = maxStr.indexOf(tmpStr);
 
-	for (i; i < s.length; i++) {
-		let curr = s[i];
-		// check if character occurs before
-		if (!characters[curr] && characters[curr] != 0) {
-			characters[curr] = i;
-		} else {
-			current_len = i - start > current_len ? i - start : current_len;
-			// start only moves forward
-			start = characters[curr] + 1 < start ? start : characters[curr] + 1;
-			characters[curr] = i;
+		if (posIndex > -1) {
+			maxStr = maxStr.substring(posIndex + 1);
 		}
+
+		maxStr += tmpStr;
+		maxLen = Math.max(maxLen, maxStr.length);
 	}
 
-	return i - start > current_len ? i - start : current_len;
+	return maxLen;
 };
