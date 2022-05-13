89642a890a1ef06e12bab2eb98ca37f81a8ce62f
Stu <stuart.feng@icloud.com>
2022-05-12 23:55:13 -0400

    Update solutions to permutations

--- Arrays/permutations.js
+++ Arrays/permutations.js
@@ -79,3 +79,73 @@ var backtrackUnique = function (result, temp, nums, used) {
 };
 
 console.log(permuteUnique([1, 1, 2]));
+
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer.
+// More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container.
+// If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+// Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function (nums) {
+	let n = nums.length,
+		i = nums.length - 1;
+
+	if (n < 2) return;
+
+	// traverse backward to find the first element s.t. nums[i-1] < nums[i]
+	// i.e. nums[i, n-1] is reversely sorted
+	while (i > 0) {
+		if (nums[i - 1] < nums[i]) break;
+
+		i -= 1;
+	}
+
+	// reverse the whole array if no such element exists
+	if (i === 0) {
+		reverseSort(nums, 0, n - 1);
+		return;
+	}
+
+	let val = nums[i - 1],
+		j = n - 1;
+
+	// search backwards for the smallest number between nums[i, n - 1]
+	while (j >= i) {
+		if (nums[j] > val) break;
+
+		j -= 1;
+	}
+
+	// swap it with the smallest number between nums[i, n-1]
+	swap(nums, j, i - 1);
+
+	// make the remaining higher position part as small as possible by reversely sorting nums[i, n-1]
+	reverseSort(nums, i, n - 1);
+	return;
+};
+
+var swap = function (nums, i, j) {
+	let temp = nums[i];
+	nums[i] = nums[j];
+	nums[j] = temp;
+};
+
+var reverseSort = function (nums, start, end) {
+	if (start > end) return;
+
+	for (let i = start; i < (start + end) / 2; i++) {
+		swap(nums, i, start + end - i);
+	}
+};
