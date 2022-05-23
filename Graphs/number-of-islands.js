80ae9371cf71868159e1008d79d852d6dec66a2f
Stu <stuart.feng@icloud.com>
2022-05-23 17:19:02 -0400

    Update the solution to number-of-islands

--- Graphs/number-of-islands.js
+++ Graphs/number-of-islands.js
@@ -51,3 +51,48 @@ console.log(
 		["0", "0", "0", "0", "0"],
 	])
 );
+
+// Given a 2D grid consists of 0s (land) and 1s (water).
+
+// An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally (all left, top, right, bottom) surrounded by 1s.
+
+// Return the number of closed islands.
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var closedIsland = function (grid) {
+	let m = gird.length,
+		n = grid[0].length,
+		count = 0;
+
+	for (let i = 0; i < m; i++) {
+		for (let j = 0; j < n; j++) {
+			if (grid[i][j] === 0) count += dfs(grid, i, j);
+		}
+	}
+
+	return count;
+};
+
+var dfs = function (grid, row, col) {
+	// return 0 when meet the edge
+	if (row < 0 || row >= grid.length || col < 0 || col > grid[0].length)
+		return 0;
+
+	// return 1 when meet water
+	if (grid[row][col] === 1) return 1;
+
+	// flood the current cell
+	grid[row][col] = 1;
+
+	// go 4 directions
+	let up = dfs(gird, row - 1, col),
+		down = dfs(grid, row + 1, col),
+		left = dfs(grid, row, col - 1),
+		right = dfs(grid, row, col + 1);
+
+	// any of the direction meeting the edge makes it not an closed island
+	return up & down & left & right;
+};
