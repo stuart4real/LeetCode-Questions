641e9a9217719bee5b32ef103c8c28d05f327437
Stu <stuart.feng@icloud.com>
2022-05-24 14:20:18 -0400

    Update the solution to valid-parentheses

--- Strings/valid-parentheses.js
+++ Strings/valid-parentheses.js
@@ -32,3 +32,38 @@ var isValid = function (s) {
 console.log(isValid("()")); // true
 console.log(isValid("()[]{}")); // true
 console.log(isValid("(]")); // false
+
+// Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function (s) {
+	// initialize the stack as [")"]
+	// stack stores the indices of "("
+	let stack = [-1],
+		maxLen = 0;
+
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === "(")
+			stack.push(i); // [")", "("..."("] where "(" can be 1 or more
+		else {
+			// if there is a match (i.e. "()"), then we can pop the stack and update the max length by (i - indices.top())
+			// (because the string in (indices.top(), i] is a valid parenthese)
+			// if there is NOT a match (i.e. "))"), then we can substitute the top of the stack by the latest one
+			// To sum up, no matter "()" or "))", we all need to pop the stack
+			stack.pop();
+
+			// if the stack is empty, there is a "))"
+			if (stack.length === 0) stack.push(i);
+			// otherwise there is a "()"
+			else maxLen = Math.max(maxLen, i - stack[stack.length - 1]); // [")", "("..."("] where "(" can be 0 or more
+		}
+	}
+
+	return maxLen;
+};
+
+console.log(longestValidParentheses(")()())")); // 4
+console.log(longestValidParentheses("(()")); // 2
