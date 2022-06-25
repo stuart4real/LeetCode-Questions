28a43c493a34849d7323edc4319250fcbbf14eb4
Stu <stuart.feng@icloud.com>
2022-06-25 15:01:51 +0800

    Update the solution to sort-list.js

--- LinkedList/sort-list.js
+++ LinkedList/sort-list.js
@@ -72,3 +72,60 @@ function merge(root, left, right) {
 
 	return root.next;
 }
+
+// Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+
+// The steps of the insertion sort algorithm:
+
+// 1. Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+// 2. At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
+// 3. It repeats until no input elements remain.
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function (head) {
+	// edge case
+	if (head === null) {
+		return head;
+	}
+
+	// create a dummy head for insertion before head
+	const dummyHead = new ListNode(0);
+	dummyHead.next = head;
+
+	// lastSorted: the last sorted node
+	// curr: the current node waiting to be inserted
+	let lastSorted = head,
+		curr = head.next;
+
+	// iterate all the node in the linked list
+	while (curr !== null) {
+		if (lastSorted.val <= curr.val) {
+			lastSorted = lastSorted.next;
+		} else {
+			// find the previous node before curr
+			let prev = dummyHead;
+			while (prev.next.val <= curr.val) {
+				prev = prev.next;
+			}
+
+			// insert curr in between prev and lastSorted
+			lastSorted.next = curr.next;
+			curr.next = prev.next;
+			prev.next = curr;
+		}
+
+		// move curr forward
+		curr = lastSorted.next;
+	}
+
+	return dummyHead.next;
+};
